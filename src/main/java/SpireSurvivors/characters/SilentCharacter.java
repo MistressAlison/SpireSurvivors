1a3286d99def66f69b00a957dc49751f1288b2c1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-18 22:39:55 -0300

    Test movement speed on Silent

--- src/main/java/SpireSurvivors/characters/SilentCharacter.java
+++ src/main/java/SpireSurvivors/characters/SilentCharacter.java
@@ -2,12 +2,12 @@ package SpireSurvivors.characters;
 
 import SpireSurvivors.entity.AbstractSurvivorPlayer;
 import SpireSurvivors.weapons.DaggerThrowWeapon;
-import SpireSurvivors.weapons.StrikeWeapon;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 
 public class SilentCharacter extends AbstractSurvivorPlayer {
     public SilentCharacter(AbstractPlayer p) {
         super(p);
+        speed*=1.2;
         weapons.add(new DaggerThrowWeapon());
     }
 }
