5d17a7784e98f81096ff5b265fc32d9c6a2bcee9
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-18 23:07:05 -0300

    Playing with flamethrower values

--- src/main/java/SpireSurvivors/weapons/FlamethrowerWeapon.java
+++ src/main/java/SpireSurvivors/weapons/FlamethrowerWeapon.java
@@ -9,12 +9,12 @@ import com.megacrit.cardcrawl.helpers.ImageMaster;
 
 public class FlamethrowerWeapon extends AbstractBurstProjectileSurvivorWeapon {
     public FlamethrowerWeapon() {
-        super(1, 1f, 10, 0.05f, 1f);
+        super(1, 1f, 12, 0.025f, 1f);
     }
 
     @Override
     public void doBurst(Vector2 lookdir) {
-        SurvivorDungeon.effectsQueue.add(new BasicProjectileAttackEffect(this, SurvivorDungeon.player.basePlayer.hb.cX, SurvivorDungeon.player.basePlayer.hb.cY, lookdir.angle(), 2000F, 0.25F, 0.5f, Color.RED.cpy(), ImageMaster.FLAME_1));
+        SurvivorDungeon.effectsQueue.add(new BasicProjectileAttackEffect(this, SurvivorDungeon.player.basePlayer.hb.cX, SurvivorDungeon.player.basePlayer.hb.cY, lookdir.angle(), 200F, 1.5F, 0.75f, Color.RED.cpy(), ImageMaster.FLAME_1));
 
     }
 
