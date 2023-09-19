d82f958cc56c8c311c9b4d81adc7bbbb8184f44f
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-18 22:33:15 -0300

    Plug in attack speed

--- src/main/java/SpireSurvivors/weapons/abstracts/AbstractSurvivorWeapon.java
+++ src/main/java/SpireSurvivors/weapons/abstracts/AbstractSurvivorWeapon.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import SpireSurvivors.dungeon.SurvivorDungeon;
 
 public abstract class AbstractSurvivorWeapon {
     public static final float CX = Settings.WIDTH/2f;
@@ -22,7 +23,7 @@ public abstract class AbstractSurvivorWeapon {
     public void update() {
         timer -= Gdx.graphics.getDeltaTime();
         if (timer <= 0f) {
-            timer = attackDelay;
+            timer = attackDelay*SurvivorDungeon.player.attackspeedModifier;
             Vector2 dir = new Vector2(InputHelper.mX - CX, InputHelper.mY - CY);
             attack(dir);
         }
