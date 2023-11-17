6dfebe79b4d4259d3981ceaaae1d9a0a79f3e378
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-17 13:03:46 -0400

    Give IC a damage boost so each character has some unique effect for now

--- src/main/java/SpireSurvivors/characters/IroncladCharacter.java
+++ src/main/java/SpireSurvivors/characters/IroncladCharacter.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 public class IroncladCharacter extends AbstractSurvivorPlayer {
     public IroncladCharacter(AbstractPlayer p) {
         super(p);
+        damageModifier *= 1.15f;
         weapons.add(new BashWeapon());
     }
 }
