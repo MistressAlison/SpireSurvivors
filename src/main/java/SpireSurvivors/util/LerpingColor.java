70fa18d69243db705f2fd9ca64c1ad16fb3f7e0e
Evelyn Veilwinter <evelyntsmg@gmail.com>
2023-11-17 23:03:38 -0500

    Fmt: Naming

--- src/main/java/SpireSurvivors/util/LerpingColor.java
+++ src/main/java/SpireSurvivors/util/LerpingColor.java
@@ -6,23 +6,23 @@ import com.badlogic.gdx.graphics.Color;
 public class LerpingColor {
     public Color value;
     private Color target;
-    public float lerp_length;
-    private float lerp_time;
+    public float lerpLength;
+    private float lerpTime;
 
     public LerpingColor(Color initial_color, float lerp_length) {
         value = initial_color;
-        this.lerp_length = lerp_length;
+        this.lerpLength = lerp_length;
     }
 
     public void update() {
         if (value.equals(target) || target == null) return;
-        if (lerp_length == 0) value = target;
-        lerp_time += Gdx.graphics.getDeltaTime();
-        value = value.lerp(target, Math.min(lerp_time/lerp_length, 1));
+        if (lerpLength == 0) value = target;
+        lerpTime += Gdx.graphics.getDeltaTime();
+        value = value.lerp(target, Math.min(lerpTime / lerpLength, 1));
     }
 
     public void setTarget(Color target) {
-        lerp_time = 0f;
+        lerpTime = 0f;
         this.target = target;
     }
 }
