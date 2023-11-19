762ff2ecffc42c0efc5422444b51846a8c3068a3
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-19 12:32:50 -0400

    Add barebones RelicCard

--- src/main/java/SpireSurvivors/cards/abstracts/AbstractRelicCard.java
+++ src/main/java/SpireSurvivors/cards/abstracts/AbstractRelicCard.java
@@ -1,9 +1,10 @@
 package SpireSurvivors.cards.abstracts;
 
+import SpireSurvivors.relics.abstracts.AbstractSurvivorRelic;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public abstract class AbstractRelicCard extends AbstractSurvivorCard {
-    public AbstractRelicCard(String id, String name, AbstractCard artCard, String rawDescription, SurvivorCardType sType) {
-        super(id, name, artCard, rawDescription, sType);
+    public AbstractRelicCard(AbstractSurvivorRelic relic, AbstractCard artCard) {
+        super(relic.id, relic.cardStrings.NAME, artCard, relic.cardStrings.DESCRIPTION, SurvivorCardType.RELIC);
     }
 }
